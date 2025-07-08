c25acfda6d008f95ab94ad9ca33e2e1eb9b65292
CloudRonn <ronnindustries1@gmail.com>
2025-07-08 19:13:53 +0300

    Updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,35 +2,29 @@ import React, { useState } from 'react';
 import '../styles/Home.css';
 import Logo from '../assets/chama-logo.png';
 import Login from '../components/Login';
+const Home = () =>{
+     const [showLogin, setShowLogin] = useState(false);
 
-const Home = () => {
-  const [showLogin, setShowLogin] = useState(false);
-
-  return (
-    <>
-    <div className={showLogin ? "home-container blur-background" : "home-container"}>
-      {/* Navbar */}
-      <div className="navbar">
-        <img src={Logo} alt="ChamaLogo" className="logo" />
-        <div className='navlinks'>
-          <a href="#">How It Works</a>
-          <a href="#">Contact us</a>
-        </div>
-        <button className='loginbutton' onClick={() => setShowLogin(true)}>Login</button>
-      </div>
-
-      {/* Hero Section */}
-      <div className="hero-section">
-        <button>Create Chama</button>
-        <button>Join Chama</button>
-      </div>
-
-      <p>This is a home page</p>
-    </div>
-     {/* Conditionally show Login Popup */}
-      {showLogin && <Login closeLogin={() => setShowLogin(false)} />}
-    </>
-  );
-};
-
+    return (
+        <> 
+            <div className="navbar">
+                <img src={Logo} alt="ChamaLogo" className="logo" />
+                <div className='navlinks'>
+                    <a href="#">How It Works</a>
+                    <a href="#">Contact us</a>   
+                </div>
+                <button className='loginbutton' onClick={() => setShowLogin(true)}>Login</button>
+            </div>
+            <div className="description">
+                <p>Where unity meets opportunity.1 Your Chama, your future.<br/></p>
+                <p className='mini-description'>Join a circle of trust. Save, lend and grow together.</p>
+            </div>
+             <div className="hero-section">
+                    <button className="createbutton">Create a Chama</button>
+                    <button className="joinbutton"> Join a Chama</button>
+            </div>
+              {showLogin && <Login closeLogin={() => setShowLogin(false)} />}
+        </>
+ );
+}
 export default Home;
