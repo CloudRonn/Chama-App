a4ed851b80642b4af0989a99010ea91cfa9fcca5
Rigoo17 <mutumasteve782@gmail.com>
2025-07-08 16:34:13 +0300

    Login component

---
+++ src/components/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+import '../styles/Login.css';
+
+const Login = ({ closeLogin }) => {
+  return (
+    <div className="login-overlay">
+      <div className="login-container">
+        <button className="close-btn" onClick={closeLogin}>×</button>
+        <div className="login-header">
+          <h2>Login</h2>
+        </div>
+
+        <input
+          type="text"
+          placeholder="Username / Email / Phone"
+          className="login-input"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="login-input"
+        />
+
+        <button className="login-submit">Login</button>
+        <div className="forgot">
+            <p>Forgot Password</p>
+        </div>
+        <p className="register-link">
+            If you don't have an account:
+            <Link to="/register"> Register</Link>
+        </p>
+           
+      </div>
+    </div>
+  );
+};
+
+export default Login;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Home.css';
 import Logo from '../assets/chama-logo.png';
-const Home = () =>{
-    return (
-        <> 
-            <div className="navbar">
-                <img src={Logo} alt="ChamaLogo" className="logo" />
-                <div className='navlinks'>
-                    <a href="#">How It Works</a>
-                    <a href="#">Contact us</a>
-                    
-                </div>
-                <button className='loginbutton'>Login</button>
-            </div>
-            <div className="hero-section">
-                    <button>Create Chama</button>
-                    <button> Join Chama</button>
-            </div>
+import Login from '../components/Login';
 
-            <p> This is a home page</p>
-        </>
-    );
+const Home = () => {
+  const [showLogin, setShowLogin] = useState(false);
 
-}
+  return (
+    <>
+    <div className={showLogin ? "home-container blur-background" : "home-container"}>
+      {/* Navbar */}
+      <div className="navbar">
+        <img src={Logo} alt="ChamaLogo" className="logo" />
+        <div className='navlinks'>
+          <a href="#">How It Works</a>
+          <a href="#">Contact us</a>
+        </div>
+        <button className='loginbutton' onClick={() => setShowLogin(true)}>Login</button>
+      </div>
+
+      {/* Hero Section */}
+      <div className="hero-section">
+        <button>Create Chama</button>
+        <button>Join Chama</button>
+      </div>
+
+      <p>This is a home page</p>
+    </div>
+     {/* Conditionally show Login Popup */}
+      {showLogin && <Login closeLogin={() => setShowLogin(false)} />}
+    </>
+  );
+};
+
 export default Home;
