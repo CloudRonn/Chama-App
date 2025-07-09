b1a39d2e00da034afba350745831f64fd6bad121
CloudRonn <ronnindustries1@gmail.com>
2025-07-09 17:54:59 +0300

    Fix routing and react setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,29 +2,46 @@ import React, { useState } from 'react';
 import '../styles/Home.css';
 import Logo from '../assets/chama-logo.png';
 import Login from '../components/Login';
-const Home = () =>{
-     const [showLogin, setShowLogin] = useState(false);
 
-    return (
-        <> 
-            <div className="navbar">
-                <img src={Logo} alt="ChamaLogo" className="logo" />
-                <div className='navlinks'>
-                    <a href="#">How It Works</a>
-                    <a href="#">Contact us</a>   
-                </div>
-                <button className='loginbutton' onClick={() => setShowLogin(true)}>Login</button>
-            </div>
-            <div className="description">
-                <p>Where unity meets opportunity.1 Your Chama, your future.<br/></p>
-                <p className='mini-description'>Join a circle of trust. Save, lend and grow together.</p>
-            </div>
-             <div className="hero-section">
-                    <button className="createbutton">Create a Chama</button>
-                    <button className="joinbutton"> Join a Chama</button>
-            </div>
-              {showLogin && <Login closeLogin={() => setShowLogin(false)} />}
-        </>
- );
-}
+const Home = () => {
+  const [showLogin, setShowLogin] = useState(false);
+
+  return (
+    <>
+     <div className="home-container">
+      <div className="navbar">
+        <img src={Logo} alt="Chama Logo" className="logo" />
+
+        <div className="navlinks">
+          <a href="#">How It Works</a>
+          <a href="#">Contact Us</a>
+        </div>
+
+        <button className="loginbutton" onClick={() => setShowLogin(true)}>
+          Login
+        </button>
+      </div>
+
+      <div className="description">
+        <p>
+          Where unity meets opportunity. Your Chama, your future.
+          <br />
+        </p>
+        <p className="mini-description">
+          Join a circle of trust. Save, lend and grow together.
+        </p>
+      </div>
+
+      <div className="hero-section">
+        <button className="createbutton">Create a Chama</button>
+        <button className="joinbutton">Join a Chama</button>
+      </div>
+
+
+      {showLogin && <Login closeLogin={() => setShowLogin(false)} />}
+        </div>
+    </>
+  );
+};
+
 export default Home;
